3f86dd43e804d726b5efec1f675fcf09f5605b10
informatica@colegioaleman.com <~f89f\VakZ+tMSwV>
2021-05-19 17:05:50 +0200

    Arduino Source Code

---
+++ 01 Arduino/Frischluft/arduino_secrets.h
@@ -0,0 +1,20 @@
+//#define SECRET_SSID "YOUR SSID"
+//#define SECRET_PASS "YOUR_SECRET_PASSWORD"
+
+//#define MQTT_USER "YOUR_MQTT_USER"
+//#define MQTT_PASS "YOUR_MQTT_PASSWORD"
+
+//#define SECRET_SSID "zuHause"
+//#define SECRET_PASS "patoAnda01_!"
+
+//#define SECRET_SSID "BYOD"
+//#define SECRET_PASS "Alberto1516"
+
+#define SECRET_SSID "Tecnologia"
+#define SECRET_PASS "tcole2020"
+
+//#define SECRET_SSID "MOVISTAR_6DA7"
+//#define SECRET_PASS "6A375011BB6F5064A1C8"
+
+#define MQTT_USER "admin"
+#define MQTT_PASS "admin"
